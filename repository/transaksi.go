d2d83950c98afa2632d8b81a093f324e4ae0845b
kevinreynaldiwork <kevinreynaldiwork@gmail.com>
2025-08-30 19:59:08 +0700

    perubahan

--- repository/transaksi.go
+++ repository/transaksi.go
@@ -83,8 +83,8 @@ func InsertTransaksi(db *sql.DB, t structs.Transaksi) error {
 	defer tx.Rollback()
 
 	_, err = tx.Exec(`INSERT INTO transaksi 
-		(kode_pelanggan, kode_barang, kode_mata_uang, jumlah_barang, total_harga, tanggal, created_at, created_by) 
-		VALUES ($1, $2, $3, $4, $5, $6, NOW(), $7)`,
+		(kode_pelanggan, kode_barang, kode_mata_uang, jumlah_barang, total_harga, tanggal, created_at, created_by, modified_at, modified_by) 
+		VALUES ($1, $2, $3, $4, $5, $6, NOW(), $7, NOW(), $8)`,
 		t.KodePelanggan, t.KodeBarang, t.KodeMataUang, t.JumlahBarang, t.TotalHarga, t.Tanggal, t.CreatedBy,
 	)
 	if err != nil {
